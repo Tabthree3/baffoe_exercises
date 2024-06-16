c55fd235106a09c7fccb6ea7373a0bf945082aeb
Tabthree3 <142275620+Tabthree3@users.noreply.github.com>
2024-06-16 12:21:33 -0400

    Update main.tf

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -10,9 +10,14 @@ resource "aws_db_instance" "RDS_instance" {
   engine_version         = "5.7"
   instance_class         = "db.t3.micro"
   db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
-  # vpc_security_group_ids = var.db_security_group_id
+  name                 = "exampledb"
+vpc_security_group_ids = var.db_security_group_id
   username               = var.db_name
   password               = var.db_password
   skip_final_snapshot    = true
+
+ tags = {
+    Name = "example-database"
+  }
 }
 
